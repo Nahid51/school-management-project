59410bab15f2b9baab5f0e4fb7660c5632dedfb9
Nahid Sheikh <mdnahidyn@gmail.com>
2024-10-29 16:50:19 +0600

    implemented loaded teachers data dynamically and also paginate them

--- components/FormModal.tsx
+++ components/FormModal.tsx
@@ -21,7 +21,7 @@ const FormModal = ({ table, type, data, id }: {
     table: "teacher" | "student" | "parent" | "subject" | "class" | "lesson" | "exam" | "assignment" | "result" | "attendance" | "event" | "announcement",
     type: "create" | "update" | "delete",
     data?: any,
-    id?: number
+    id?: string
 }) => {
 
     const size = type === "create" ? "w-8 h-8" : "w-7 h-7";
